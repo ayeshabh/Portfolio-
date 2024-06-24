bff249d6c0ba531e8a5a8b37e32ff64696171f68
Ayesha Bhangu <ayesha.bhangu@outlook.com>
2024-06-24 16:32:59 -0400

    uploaded resume file

--- components/intro.tsx
+++ components/intro.tsx
@@ -84,7 +84,7 @@ export default function Intro() {
 
         <a
           download
-          href="/Duraiz-Haider-Resume.pdf"
+          href="/Ayesha-Bhangu-Resume.pdf"
           className="group   bg-white  px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
         >
           Download RESUME{" "}
